bed215bf7fec5ed13f971d76259b4ea090609dfa
randy.mowen@gmail.com <randy.mowen@gmail.com>
2020-11-11 06:25:56 -0800

    V6

---
+++ V6/DEMO_RS/Create_index.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  DDL for Index INDEX1
+--------------------------------------------------------
+
+  CREATE INDEX INDEX1 ON CONTINENTS ("CONTINENTID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS" ;
+
