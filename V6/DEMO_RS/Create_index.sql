3d72aa1ab4b993f0a9e8af5e160ee9c442720cc3
randy.mowen@gmail.com <randy.mowen@gmail.com>
2020-11-25 08:30:52 -0800

    remove V6

--- V6/DEMO_RS/Create_index.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  DDL for Index INDEX1
---------------------------------------------------------
-
-  CREATE INDEX INDEX1 ON CONTINENTS ("CONTINENTID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE "USERS" ;
-
